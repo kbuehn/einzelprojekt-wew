00d3bde16982c30293bc94410f5042451061f957
Katharina <katharina.buehn@stud.fh-campuswien.ac.at>
2022-09-13 19:20:26 +0200

    commit

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,44 +1,30 @@
 const path = require('path');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// eslint-disable-next-line import/no-extraneous-dependencies
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  // 1
-  // Use the src/index.ts file as entry point to bundle it.
-  // If the src/index.ts file imports other JS files,
-  // bundle them as well
   entry: path.resolve(__dirname, './src/scripts/index.ts'),
 
-  // 2
-  // The bundles source code files shall result in a bundle.js file
-  // in the /dist folder
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "./dist"),
   },
-  // 3
-  // The /dist folder will be used to serve our application
-  // to the browser
+
   devServer: {
     static: path.resolve(__dirname, './dist'),
   },
-  // 4
-  // Add plugins for webpack here
+
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Web Engineering Einzelprojekt',
+      title: 'Web Engineering Einzelprojekt DEV',
       template: path.resolve(__dirname, './src/index.html'),
     }),
     new ESLintPlugin(),
   ],
-  // 5
-  // Integrate Babel in the build process
-  // Define which files to use the loader
+
   module: {
     // configuration regarding modules
     rules: [
@@ -64,7 +50,6 @@ module.exports = {
     ],
   },
   resolve: {
-    // options for resolving module requests
     extensions: ['*', '.js', '.ts'], // files to load
   },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -4,38 +4,22 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'production',
-    // 1
-    // Use the src/index.ts file as entry point to bundle it.
-    // If the src/index.ts file imports other JS files,
-    // bundle them as well
     entry: path.resolve(__dirname, './src/scripts/index.ts'),
-    // 2
-    // The bundles source code files shall result in a bundle.js file
-    // in the /dist folder
     output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./dist"),
     },
-    // 3
-    // The /dist folder will be used to serve our application
-    // to the browser
     devServer: {
         static: path.resolve(__dirname, './dist'),
     },
-    // 4
-    // Add plugins for webpack here
     plugins: [
         new CleanWebpackPlugin,
         new HtmlWebpackPlugin({
-            title: "Basic Webpack Setup",
+            title: "Web Engineering Einzelprojekt PROD",
             template: path.resolve(__dirname, './src/index.html'),
         })
     ],
-    // 5
-    // Integrate Babel in the build process
-    // Define which files to use the loader
     module: {
-        // configuration regarding modules
         rules: [
             {
                 test: /\.(js)$/,
@@ -59,7 +43,6 @@ module.exports = {
         ],
     },
     resolve: {
-        // options for resolving module requests
         extensions: ['*', '.js', '.ts'], // files to load
     },
 };
